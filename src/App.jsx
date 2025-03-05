c306ebcd100ff54914cddf890004b661e7fcdfb6
Ajiboye Elijah <ajiboyeelijah242@gmail.com>
2025-03-05 11:34:04 +0100

    commit update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Navbar from './pages/Navbar';
 import Portfolio from './pages/Portfolio';
-import AboutMe from './pages/AboutMe'; 
+import AboutMe from './pages/aboutMe';
 import Testimonials from './pages/Testimonials';
 import Skill from './pages/Skill';
 import Home from './pages/Home';
